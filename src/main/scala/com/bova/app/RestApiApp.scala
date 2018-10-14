8762f1f6caae0c2e8fdaaf61670c380d9e34d806
myk <bova.mykola@gmail.com>
2018-10-15 00:00:26 +0300

    Press Enter to stop server

--- src/main/scala/com/bova/app/RestApiApp.scala
+++ src/main/scala/com/bova/app/RestApiApp.scala
@@ -4,15 +4,24 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 
+import scala.concurrent.ExecutionContextExecutor
+
 object RestApiApp extends RestApi {
 
   def main(args: Array[String]) {
 
-    implicit val actorSystem: ActorSystem = ActorSystem("system")
+    implicit val system: ActorSystem = ActorSystem("system")
     implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
+    implicit val ec: ExecutionContextExecutor = system.dispatcher
 
-    Http().bindAndHandle(route,"localhost",8080)
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
-    println("server started at 8080")
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    scala.io.StdIn.readLine()
+
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.shutdown())
+
   }
 }
