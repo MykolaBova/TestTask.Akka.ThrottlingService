b9a4c27ebe157eb977f11fad2c5ab77cd10d621a
myk <bova.mykola@gmail.com>
2018-10-15 22:58:07 +0300

    Get "Authorization" HTTP header

--- src/main/scala/com/bova/app/RestApi.scala
+++ src/main/scala/com/bova/app/RestApi.scala
@@ -1,15 +1,33 @@
 package com.bova.app
 
-import akka.http.scaladsl.server.Directives.{complete, get, pathSingleSlash}
+import akka.http.scaladsl.model.HttpHeader
+import akka.http.scaladsl.server.Directives.{get, pathSingleSlash}
 import akka.http.scaladsl.server.Route
+
+import scala.collection.immutable
 
 trait RestApi {
   val route: Route =
     pathSingleSlash {
-      get {
-        complete {
-          "Hello world :)"
+      get { ctx =>
+        val headers: immutable.Seq[HttpHeader] = ctx.request.headers
+        val token: String = getHeaderByName(headers, "Authorization")
+
+        ctx.complete {
+          s"Hello world :), token == $token"
         }
       }
     }
+
+  // TODO: quick and dirty hack To replace with standard way of retrieving HTTP headers later
+  def getHeaderByName(headers: immutable.Seq[HttpHeader], headerName: String) : String = {
+    headers
+      .toArray
+      .map(it => it.toString()
+        .split(":", 2))
+      .filter(_.length == 2 )
+      .map(x => Tuple2(x(0), x(1)))
+      .filter(x => x._1 == headerName)(0)
+      ._2
+  }
 }
